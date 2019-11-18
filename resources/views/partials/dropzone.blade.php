e119dc8354bd496bfb115dc272638d55546e259f
Arash-Rostami <arashrostami@time-gr.com>
2019-11-18 00:38:26 -0800

    another

--- resources/views/partials/dropzone.blade.php
+++ resources/views/partials/dropzone.blade.php
@@ -1,25 +1,20 @@
 
+<form action="/admin/image"
+      class="dropzone"
+      id="mydropzone"
+      method="post"
+      enctype="multipart/form-data">
+    @csrf
+    <input type="hidden" id="prod" name="productid" value="{{$product->id}}">
 
-    <form action="/admin/image" class="dropzone" id="mydropzone" method="post">
-        @csrf
-        <div class="form-group col-md-4 ">
-            <label for=""> قبل از بارگذاری عکس و ذخیره ابتدا محصول را انتخاب کنید</label>
-            <select class="form-control">
 
-                @foreach($product as $products)
-                    <option id="{{$products->id}}">
-                        {{ $products->name }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <div class="fallback">
-            <input name="file" type="file" multiple/>
-            <input type="submit" value="ذخیره" class="btn btn-primary"
-                   id="btnAddImage"
-            >
-        </div>
-    </form>
+    <div class="fallback">
+        <input name="file" type="file" multiple/>
+
+    </div>
+</form>
+
+
 
 
 
