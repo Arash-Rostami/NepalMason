19bd2ea31c611fac6c2e1e06dac22b1dac259780
Arash-Rostami <arashrostami@time-gr.com>
2020-04-07 19:09:14 -0700

    latest changes
    last

--- app/Http/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-function flashMessage($title, $message, $type)
-{
-    $flashMessageClass = App('App\Http\Utilities\flashMessageClass');
-    $flashMessageClass->flashMessageClassFunc($title, $message, $type);
-}
-
-
-
--- resources/views/admin/category.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.layout')
-
-@section('content')
-
-
-    <categrycreate parentcategory="{{ $parents }}" maincategory="{{ $categories }}"> </categrycreate>
-
-@stop
